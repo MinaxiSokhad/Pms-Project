6f3a4c6f640f3c156797fd33caa6c3c265e5f5ed
MinaxiSokhad <minaxisokhad@gmail.com>
2024-09-19 18:56:38 +0530

    .

--- deletecustomers.php
+++
@@ -1,6 +0,0 @@
-<?php include "includes/_header.php"; ?>
-<title>Customers</title>
-
-<?php
-include "includes/_footer.php";
-?>
--- editcustomers.php
+++
@@ -1,5 +0,0 @@
-<?php include "includes/_header.php"; ?>
-<title>Customers</title>
-
-
-<?php include "includes/_footer.php"; ?>
